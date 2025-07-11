e86cbce3d6f5e3d69c780a2db9892535ab13276a
Depto Roy <155212370+Depto2007@users.noreply.github.com>
2025-07-11 14:26:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
+CORS(app)  # ✅ Allow CORS for all routes
 
 @app.route('/')
 def home():
